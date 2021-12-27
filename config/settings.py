f4a02e0d40e30ad0a199abb38edb73c4942e0b51
khhan <khhan7813@gmail.com>
2021-12-27 11:42:36 +0900

    Set allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6w%#xc*av+s2@39$vqr=d4d&ri!wf^rw8991v7n1_+efn887(_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.164.16.127']
 
 
 # Application definition
